1b21c86615caf7b49c0b971db7046fc4af87c002
Maxessien <essienmax484@gmail.com>
2026-05-18 16:16:58 +0100

    feat: enhance type definitions for Express request and improve code formatting

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,28 @@
 import { config } from "dotenv";
-config()
+import {} from "multer";
+config();
 
-import app from './configs/serverConfig.js';
+import app from "./configs/serverConfig.js";
 import logger from "./utils/logger.js";
+import { DecodedIdToken } from "firebase-admin/auth";
 
+interface DecodedTokenWithClaims extends DecodedIdToken {
+  role: "user" | "admin";
+}
 
+declare global {
+  namespace Express {
+    interface Request {
+      auth: DecodedTokenWithClaims;
+      images: { url: string; publicId: string }[];
+      file?: Multer.File;
+      files?: { [fieldname: string]: Multer.File[] } | Multer.File[];
+    }
+  }
+}
 
-const PORT = process.env.PORT || 5050
+const PORT = process.env.PORT || 5050;
 
-app.listen(Number(PORT), "0.0.0.0", ()=>{
-    logger.info("App listening on", PORT)
-})
+app.listen(Number(PORT), "0.0.0.0", () => {
+  logger.info("App listening on", PORT);
+});
